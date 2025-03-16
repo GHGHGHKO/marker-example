92c0517545521f34114ec3c4951868976c9f758f
GHGHGHKO <gudrb963@gmail.com>
2025-03-16 22:34:30 +0900

    feat: Initial commit

---
+++ src/main/java/com/ghghghko/marker_example/MarkerExampleApplication.java
@@ -0,0 +1,13 @@
+package com.ghghghko.marker_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkerExampleApplication.class, args);
+	}
+
+}
